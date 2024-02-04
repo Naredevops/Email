92c096b918f76e7aabf097b89a9b0085abc52d50
Naredevops <48598624+Naredevops@users.noreply.github.com>
2024-02-04 17:53:05 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -2,15 +2,19 @@ $EmailFrom = "narender5t9.devops@gmail.com"
 $EmailTo = "narender6t9.devops@gmail.com"
 $Subject = "Test Gmail from GitHub"
 $Body = "This is a test email sent from GitHub Actions using PowerShell."
+$Signature = "Regards, Narender Reddy"
 $SMTPServer = "smtp.gmail.com"
 $SMTPPort = "587"
 $SMTPUsername = "narender5t9.devops@gmail.com"
 $SMTPPassword = "bbhb canf dpcd creb" # Use your Gmail app password here
 
+# Concatenate message body and signature
+$FullBody = "$Body`n`n$Signature"
+
 # Create email message
 $Message = New-Object System.Net.Mail.MailMessage $EmailFrom, $EmailTo
 $Message.Subject = $Subject
-$Message.Body = $Body
+$Message.Body = $FullBody
 $Message.IsBodyHTML = $true
 
 # Configure SMTP client
