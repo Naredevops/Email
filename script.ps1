de067f3887e34a4904515babaca693533c896e5d
Naredevops <48598624+Naredevops@users.noreply.github.com>
2024-02-04 14:36:34 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,15 +1,16 @@
 $EmailFrom = ${{ secrets.SMTP_USERNAME }}
 $EmailTo = "narender6t9.devops@gmail.com"
-$secrets.SMTP_PASSWORD = "bbhb canf dpcd creb"
-$secrets.SMTP_USERNAME = "narender5t9.devops@gmail.com"
+$SMTPPassword = "${{ secrets.SMTP_PASSWORD }}"
+$SMTPUsername = "narender5t9.devops@gmail.com"
 $Subject = "PF Account UAN not activated"
-$Body = "Hi PF Team,
-
+$Body = @"
+Hi PF Team,
 
 It's been a long time, Can you please provide feedback on this item.
 
 Regards
-Narender Reddy."
+Narender Reddy.
+"@
 $SMTPServer = "smtp.gmail.com"
 $SMTPPort = 587
 
@@ -22,12 +23,12 @@ $Message.IsBodyHTML = $true
 # Configure SMTP client
 $SMTPClient = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort)
 $SMTPClient.EnableSsl = $true
-$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($EmailFrom, ${{ secrets.SMTP_PASSWORD }})
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($SMTPUsername, $SMTPPassword)
 
 # Send the email
 try {
     $SMTPClient.Send($Message)
     Write-Output "Email sent successfully."
 } catch {
-    Write-Error "Failed to send email: $_.Exception.Message"
+    Write-Error "Failed to send email: $($_.Exception.Message)"
 }
